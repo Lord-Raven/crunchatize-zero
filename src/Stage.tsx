c90ee1d49d949693af28a62f55c8a05e744b4ab1
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-08-28 02:44:18 +0000

    Tweaking template.

--- src/Stage.tsx
+++ src/Stage.tsx
@@ -21,11 +21,11 @@ interface SaveState {
 const DEFAULT_OUTCOME_TEMPLATE = '<div style="--failure: 255, 0, 0; --mixed: 255, 140, 0; --success: 60, 179, 113; --critical: 176, 224, 230; margin: 0.5rem; ' +
     'border: 1px solid rgb(var(--{{total_class}})); border-radius: 8px; padding: 0.75rem 1rem; background: linear-gradient(to right, rgba(var(--{{total_class}}), 0.2), #0003); box-shadow: 0 2px 4px #0003; ' +
     'font-family: system-ui, sans-serif; text-shadow: 2px 2px 4px #0003; font-size: 2rem; line-height: 2rem;">' +
-    '<div style="display: flex; justify-content: center; color: #bbb; font-size: 1rem;"><b><i>{{content}}</b></i></div>' +
+    '<div style="display: flex; justify-content: center; color: #bbb; font-size: 1.5rem;"><b><i>{{content}}</b></i></div>' +
     '<div style="display: flex; justify-content: center; hidden: {{has_outcome}};">' +
     '<span style="color: rgb(var(--{{dice1_class}}));"><span style="font-size: 2.5rem;">{{dice1_emoji}}</span> {{dice1_value}}</span>' +
     '<span style="color: rgb(var(--{{dice2_class}}));"> + <span style="font-size: 2.5rem;">{{dice2_emoji}}</span> {{dice2_value}}</span>' +
-    '<span style="color: rgb(var(--{{modifier_class}}));"> {{modifier_sign}} {{modifier_absolute}} <sup style="font-size: 1rem;">(difficulty)</sup></span>' +
+    '<span style="color: rgb(var(--{{modifier_class}}));"> {{modifier_sign}} {{modifier_absolute}}<sup style="font-size: 1rem; vertical-align: top;">(difficulty)</sup></span>' +
     '<span style="color: rgb(var(--{{total_class}}));"> = {{total}} ({{total_label}})</span></div></div>';
 
 export class Stage extends StageBase<InitStateType, ChatStateType, MessageStateType, ConfigType> {
